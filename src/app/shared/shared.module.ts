e038b04a6f4ba1b8e8f5b9f8f57c3ae51395f264
Yilver7u7 <yilversgg@gmail.com>
2024-09-21 23:31:47 -0500

    Prepared all the components that we'll need to make the appHero

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    // Shared components, services, directives, to export into appModule
+  ]
+})
+export class SharedModule { }
