a47f822287313cf7a97079b78fb7da04d855af49
Yilver7u7 <yilversgg@gmail.com>
2024-09-21 23:39:41 -0500

    It had finished the majority of components and module to develop this HeroApp

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  declarations: [
+    Error404PageComponent
   ],
   exports: [
     // Shared components, services, directives, to export into appModule
+    Error404PageComponent,
+
   ]
 })
 export class SharedModule { }
