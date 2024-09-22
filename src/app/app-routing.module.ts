2d9c1795d94f93a489d94bc12bfab5de66a4e27a
Yilver7u7 <yilversgg@gmail.com>
2024-09-22 01:43:40 -0500

    Added whole the routes, to the each component and rendering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
+
+// Dominio por defecto donde ingresan es un path basio
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  // Cuando la ruta esta vacia redirige al home deseado
+  {
     path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full' //Para evitar problemas de espaciado en las rutas
+  },
+  // Cuando se escribe cualuier otro path que sea desconocido redirrige al 404
+  {
+    path: '**',
+    redirectTo: '404'
   }
 ];
 
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'add-hero',component: NewPageComponent },
+      // Para la visualizacion de un componente en todo el layout
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id',component: NewPageComponent },
+      { path: 'list',component: ListPageComponent },
+      // Este va recibir un ID
+      // Dentro del componente podremos verificar si este ID existe
+      { path: ':id',component: HeroPageComponent },
+      { path: '**',component: ListPageComponent },
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
