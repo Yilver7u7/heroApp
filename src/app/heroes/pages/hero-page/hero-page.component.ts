bd19d219aa49bc66ed0bf7df22a6a219f616a1a5
Yilver7u7 <yilversgg@gmail.com>
2024-09-24 13:17:34 -0500

    Add and method into the service to manage the error while someone was going to find undefied hero

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HerosService } from '../../services/heroes.services';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'heroes-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  public hero?: Hero;
+
+  constructor(
+    private herosService: HerosService,
+    // Cuando la ruta esta activa permite hacer un subcribe
+    private activiteRoute: ActivatedRoute,
+    // Con esto si el valor del id del Hero es undefied yo lo regreso a la ventana anterior
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.activiteRoute.params
+    .pipe(
+      // Me permite tomar los params y usarlos
+      switchMap( ({ id }) => this.herosService.getHeroById(id)),
+    )
+    .subscribe( hero => {
+      if( !hero ) return this.router.navigateByUrl('heroes/list');
+      this.hero = hero;
+      return;
+    })
+  }
+
+
 
 }
--- src/app/heroes/services/heroes.services.ts
+++ src/app/heroes/services/heroes.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -14,6 +14,16 @@ export class HerosService {
   // En point HttpClient
   getHeroes():Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById( id: string ): Observable<Hero | undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+    // I consider this is a try catch buy with Observable in TS
+    .pipe(
+      catchError( error => of(undefined) )
+    );
+
+
   }
 
 
