d43460f7f07724dbf876fa7122da05ea36f731c9
Yilver7u7 <yilversgg@gmail.com>
2024-09-29 21:15:54 -0500

    Add the FormGropu and linked with the HTML

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
+import { HerosService } from '../../services/heroes.services';
 
 @Component({
   selector: 'heroes-new-page',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NewPageComponent {
+
+  // Esta es la forma de hacer formularios reactivos
+// Maneja las mimsa cosideraciones de la interface
+  public heroForm = new FormGroup({
+  id:               new FormControl<string>(''),
+  superhero:        new FormControl<string>('', { nonNullable: true }), //Esto es un required
+  publisher:        new FormControl<Publisher>( Publisher.DCComics),
+  alter_ego:        new FormControl(''),
+  first_appearance: new FormControl(''),
+  characters:       new FormControl(''),
+  alt_img:          new FormControl(''),
+  });
+
+
+  public publishers = [
+    { id:'DC Comics', value:'DC - Comics'},
+    { id:'Marvel Comics ', value:'Marvel - Comics'},
+  ]
+
+  constructor( private herosService: HerosService) {}
+
+  
+
+    onSumit(): void {
+      console.log( {
+        formIsValid: this.heroForm.valid,
+        value: this.heroForm.getRawValue()
+      })
+    }
+
 
 }
--- src/app/heroes/services/heroes.services.ts
+++ src/app/heroes/services/heroes.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -27,6 +27,34 @@ export class HerosService {
   getSuggestions( query: string ): Observable<Hero[]>{
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
   }
+
+  // La data se manda como segundo argumento
+  addHero( hero: Hero ): Observable<Hero>{
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero)
+  }
+
+  // Solo actualiza los datos que vienen del objeto
+  updateHero( hero: Hero ): Observable<Hero>{
+    if( !hero.id ) throw Error("Hero is required");
+    // Se manda el Id para encontrar el objero y luego los
+    // datos o parametros que se van a actualizar
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes`, hero)
+  }
+
+  // En estos casos retornamos un booleano para asegurarnos
+  // que el valor haya sido eliminado correctamente
+  deleteHeroById ( id: string): Observable<boolean>{
+
+    return this.http.delete(`${ this.baseUrl }/heroes`)
+    .pipe(
+      // Si la consulta sale mal regresara un false
+      catchError( err => of ( false ) ),
+      // Si por el contrario llega hasta este punto devuelve un true
+      map( resp => true)
+    )
+  }
+
+
 
 
 }
